a7722f19ad1e7acc79289e26b622ec43a390d38b
luis <luistenan@gmail.com>
2022-10-21 11:31:00 -0400

    Update Personal and General components importation to current names

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // App.js
 import React, { Component } from "react";
-import General from "./components/General";
+import MainInformation from "./components/MainInformation";
 import Experience from "./components/Experience";
 import "./styles/styles.css";
-import Personal from "./components/Personal";
+import SecondaryInformation from "./components/SecondaryInformation";
 import "./styles/App.css";
 
 class App extends Component {
@@ -15,9 +15,9 @@ class App extends Component {
     return (
       <div>
         <div className="centerContainer">
-          <Personal />
+          <SecondaryInformation />
           <div className="rightCenterContainer">
-            <General />
+            <MainInformation />
             <h2>Education</h2>
             <Experience />
           </div>

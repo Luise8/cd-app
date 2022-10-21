38ee0b998a07b6b76b659dc74e2d6678d6621a03
luis <luistenan@gmail.com>
2022-10-21 12:35:19 -0400

    Update MainInformation imports and state and body
    Import ModalForm and ModalConfirmation.
    Set state to can work with them.
    Add const with informtation to ModalForm.
    Add all of the elements: h1, h2, p, buttons.

--- src/components/MainInformation.js
+++ src/components/MainInformation.js
@@ -1,56 +1,69 @@
 // MainInformation.js MainInformation information
 import React, { Component } from "react";
+import ModalForm from "./ModalForm";
+import ModalConfirmation from "./ModalConfirmation";
 
 class MainInformation extends Component {
   constructor() {
     super();
 
     this.state = {
-      fullName: "",
-      title: "" /*Professional title placeholder*/,
-      resume: "",
+      // Item
+      itemMainInformation: [
+        { key: "fullName", text: "Add your full name" },
+        {
+          key: "positionTitle",
+          text: "Add your Position Title",
+        },
+        { key: "resume", text: "Add your resume" },
+      ],
+      isActive: false, // Modal boolean
     };
   }
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.id]: e.target.value,
-    });
-  };
-
   render() {
-    const { fullName, title, resume } = this.state;
+    const { isActive, itemMainInformation } = this.state;
+    const form = {
+      inputList: [
+        {
+          titleLabel: "Enter your full Name:",
+          keyStateName: "fullName",
+          typeInput: "text",
+          indexStateValue: 0,
+        },
+        {
+          titleLabel: "Enter your position Title:",
+          keyStateName: "positionTitle",
+          typeInput: "text",
+          indexStateValue: 1,
+        },
 
+        {
+          titleLabel: "Enter your resume",
+          keyStateName: "resume",
+          typeInput: "text",
+          indexStateValue: 2,
+        },
+      ],
+      btnList: {
+        btnOne: "Save",
+        btnTwo: "Cancel",
+      },
+    };
     return (
-      <div>
-        <div>
-          <label htmlFor="fullName">Enter your name </label>
-          <input
-            onChange={this.handleChange}
-            value={fullName}
-            type="text"
-            id="fullName"
-          />
+      <>
+        <div className="container-main-information">
+          <h1>{itemMainInformation[0].text}</h1>
+          <h2>{itemMainInformation[1].text}</h2>
+          <p>{itemMainInformation[2].text}</p>
+          <button className="btn-main-inf" onClick={this.handleToggle}>
+            EDIT
+          </button>
+          <button className="btn-main-inf" onClick={this.handleToggleDeleteAll}>
+            DEFAULT RESTORE
+          </button>
         </div>
-        <div>
-          <label htmlFor="title">Professional title </label>
-          <input
-            onChange={this.handleChange}
-            value={title}
-            type="text"
-            id="title"
-          />
-        </div>
-        <div>
-          <label htmlFor="resume">Enter your resume </label>
-          <input
-            onChange={this.handleChange}
-            value={resume}
-            type="text"
-            id="resume"
-          />
-        </div>
-      </div>
+      </>
     );
   }
 }

4100ee40382e3aa316e2a7ed1d2e4c446f4b835a
luis <luistenan@gmail.com>
2022-10-28 19:52:23 -0400

    Add basic structure of Skills component
    Define state, add necesary imports, and add buttons

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,10 @@
 // Skills.js
-
 import React, { Component } from "react";
 import "../styles/Skills.css";
+import OverviewArrayObjects from "./OverviewArrayObjects";
+import ModalForm from "./ModalForm";
+import ModalConfirmation from "./ModalConfirmation";
+import uniqid from "uniqid";
 
 class Skills extends Component {
   constructor(props) {
@@ -9,11 +12,77 @@ class Skills extends Component {
 
     this.state = {
       // Item
+      skillItem: [
+        { id: uniqid() },
+        { key: "skill", text: "", tag: "p", className: "para" },
+      ],
+      skillList: [], // Array of items
+      isActive: false, // Modal boolean
+      isActiveEdit: false, // Modal boolean
+      activeDeleteAll: false, // Modal boolean
+      itemSelectedIndex: -1, // pointer expList item for edit item
     };
+
+    /*     this.removeItem = this.removeItem.bind(this);
+    this.changeItem = this.changeItem.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+    this.onSubmitItem = this.onSubmitItem.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleToggleEdit = this.handleToggleEdit.bind(this);
+    this.handleToggleDeleteAll = this.handleToggleDeleteAll.bind(this);
+    this.clearAll = this.clearAll.bind(this); */
   }
 
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div className="container-title-skills">
+          <h3>SKILLS</h3>
+          <button className="btn-add-skill" /* onClick={this.handleToggle} */>
+            ADD
+          </button>
+          <button
+            className="btn-add-skill"
+            /* onClick={this.handleToggleDeleteAll} */
+          >
+            DELETE ALL
+          </button>
+        </div>
+        {/* Show all the items experience on the screen */}
+        {/*  <OverviewArrayObjects
+          itemList={}
+          removeItem={this.removeItem}
+          changeItem={this.changeItem}
+        /> */}
+        {/* Modal confirmation to delete all */}
+        {/*  <ModalConfirmation
+          modalActive={activeDeleteAll}
+          btnOneFunction={this.clearAll}
+          btnTwoFunction={this.handleToggleDeleteAll}
+          text=""
+        /> */}
+        {/* Modal edit fornm */}
+        {/* <ModalForm
+          modalActive={isActiveEdit}
+          inputsList={form.inputsList}
+          btnList={form.btnList}
+          stateInputValues={}
+          btnTwoFunction={this.handleToggleEdit}
+          btnOneFunction={this.onsubmitEditItem}
+          onChange={this.handleChange}
+        /> */}
+        {/* Modal fornm */}
+        {/* <ModalForm
+          modalActive={isActive}
+          inputsList={form.inputsList}
+          btnList={form.btnList}
+          stateInputValues={}
+          btnTwoFunction={this.handleToggle}
+          btnOneFunction={this.onSubmitItem}
+          onChange={this.handleChange}
+        /> */}
+      </div>
+    );
   }
 }
 

a98d474a43c46a297494fbeee5693c95b49e4047
luis <luistenan@gmail.com>
2022-11-01 19:42:32 -0400

    Add example components
    example component have default information to Skills, Experience, and Education when the page load

---
+++ src/components/example.js
@@ -0,0 +1,116 @@
+const example = {
+  skills: [
+    [
+      { id: "skill-1" },
+      { key: "skill", text: "CSS", tag: "p", className: "para" },
+    ],
+    [
+      { id: "skill-2" },
+      { key: "skill", text: "JavaScript", tag: "p", className: "para" },
+    ],
+    [
+      { id: "skill-3" },
+      { key: "skill", text: "HTML", tag: "p", className: "para" },
+    ],
+    [
+      { id: "skill-4" },
+      { key: "skill", text: "React", tag: "p", className: "para" },
+    ],
+  ],
+  education: [
+    [
+      { id: "education-1" },
+      {
+        key: "institution",
+        text: "Harvard University",
+        tag: "h3",
+        className: "title",
+      },
+      { key: "degree", text: "Computer Science", tag: "p", className: "para" },
+      {
+        key: "dateFrom",
+        text: "2010-11-15",
+        tag: "p",
+        className: "para lowOpacity date",
+      },
+      {
+        key: "dateTo",
+        text: "2015-11-15",
+        tag: "p",
+        className: "para lowOpacity date dateTo",
+      },
+    ],
+  ],
+  experience: [
+    [
+      { id: "experience-1" },
+      {
+        key: "positionTitle",
+        text: "Web Developer",
+        tag: "h3",
+        className: "title",
+      },
+      { key: "companyName", text: "Company 1", tag: "p", className: "para" },
+      {
+        key: "dateFrom",
+        text: "2015-12-10",
+        tag: "p",
+        className: "para lowOpacity date",
+      },
+      {
+        key: "dateTo",
+        text: "2017-05-15",
+        tag: "p",
+        className: "para lowOpacity date dateTo",
+      },
+      {
+        key: "Location",
+        text: "Remote",
+        tag: "p",
+        className: "para lowOpacity",
+      },
+      {
+        key: "mainTasks",
+        text: "Make great things",
+        tag: "p",
+        className: "para",
+      },
+    ],
+    [
+      { id: "experience-2" },
+      {
+        key: "positionTitle",
+        text: "Back-end Developer",
+        tag: "h3",
+        className: "title",
+      },
+      { key: "companyName", text: "Company 2", tag: "p", className: "para" },
+      {
+        key: "dateFrom",
+        text: "2017-05-20",
+        tag: "p",
+        className: "para lowOpacity date",
+      },
+      {
+        key: "dateTo",
+        text: "2022-11-01",
+        tag: "p",
+        className: "para lowOpacity date dateTo",
+      },
+      {
+        key: "Location",
+        text: "Remote",
+        tag: "p",
+        className: "para lowOpacity",
+      },
+      {
+        key: "mainTasks",
+        text: "Make also great things",
+        tag: "p",
+        className: "para",
+      },
+    ],
+  ],
+};
+
+export default example;
